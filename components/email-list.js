7d1c00277fe29130079894b61233d790af588a0a
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-02-23 18:08:35 -0800

    initial commit

--- components/email-list.js
+++ components/email-list.js
@@ -3,7 +3,7 @@ import MailBox from './mailbox';
 
 export default function EmailList(props) {
     const emails = Object.keys(props.emails).map(function(inboxOrSpam, index) {
-        const email = props.emails[inboxOrSpam];
+        const emails = props.emails[inboxOrSpam];
         return (
             <li key={index}>
                 <MailBox inbox={email.inbox} spam={email.name} />
